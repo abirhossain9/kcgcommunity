587f56b512109ae8c531d9a531f335f5cc7aef16
Md Abir Hossain <abir.hossain04@northsouth.edu>
2025-01-06 19:45:54 +0900

    primary key in role added

--- database/migrations/2024_06_28_175920_create_role_user_pivot_table.php
+++ database/migrations/2024_06_28_175920_create_role_user_pivot_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')
                 ->references('id')
                 ->on('users')
